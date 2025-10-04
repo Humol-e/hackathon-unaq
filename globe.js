08ced935a8522014a6eed7cecde132e5bc93d994
Humol-e <luisemiliano.castroreza@gmail.com>
2025-10-04 14:09:16 -0600

    Mejorado el readme

--- globe.js
+++ globe.js
@@ -43,7 +43,7 @@ DAT.Globe = function(container, opts) {
         'void main() {',
           'vec3 diffuse = texture2D( texture, vUv ).xyz;',
           'float intensity = 1.05 - dot( vNormal, vec3( 0.0, 0.0, 1.0 ) );',
-          'vec3 atmosphere = vec3( 1.0, 1.0, 1.0 ) * pow( intensity, 3.0 );',
+          'vec3 atmosphere = vec3( 1.0, 1.0, 1.0 ) * pow( intensity, 6.0 );',
           'gl_FragColor = vec4( diffuse + atmosphere, 1.0 );',
         '}'
       ].join('\n')
