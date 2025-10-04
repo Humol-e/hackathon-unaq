1f67b520504d06d37c1568e4abe2a3d7d2bd476d
Humol-e <luisemiliano.castroreza@gmail.com>
2025-10-04 17:31:34 -0600

    cambiamos mapa a png y comenzamos extraccion de json

--- globe.js
+++ globe.js
@@ -103,7 +103,7 @@ DAT.Globe = function(container, opts) {
     shader = Shaders['earth'];
     uniforms = THREE.UniformsUtils.clone(shader.uniforms);
 
-    uniforms['texture'].value = THREE.ImageUtils.loadTexture('mapa.jpg');
+    uniforms['texture'].value = THREE.ImageUtils.loadTexture('mapa.png');
 
     material = new THREE.ShaderMaterial({
 
