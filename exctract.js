0ce0dfa73d363a08de6dfd64fc84b31030ef05db
Humol-e <luisemiliano.castroreza@gmail.com>
2025-10-05 02:05:16 -0600

    Merge branch 'master' of https://github.com/Humol-e/hackathon-unaq

--- exctract.js
+++ exctract.js
@@ -3,7 +3,7 @@ const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
 const PORT = 3000;
-
+app.use(express.static(__dirname));
 app.get('/asteroids', async (req, res) => {
     const page = req.query.page || 0;
     const apiKey = process.env.API_KEY;
